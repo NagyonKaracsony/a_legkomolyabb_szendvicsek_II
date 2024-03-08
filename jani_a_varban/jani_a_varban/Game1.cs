34aa731daf0b955f2fba7f7b6cc191f621d798da
NagyonKaracsony <nagyeliot2004@gmail.com>
2024-03-08 07:58:44 +0100

    updated game class

--- jani_a_varban/jani_a_varban/Game1.cs
+++ jani_a_varban/jani_a_varban/Game1.cs
@@ -2,11 +2,13 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Diagnostics;
 namespace jani_a_varban
 {
     public class Game1 : Game
     {
-        Player player;
+        public static Player player;
+        public static GameWindow GameWindow { get; private set; }
         public static Texture2D playerTexture;
         public static Texture2D enemyTexture;
         public static Texture2D floorTexture;
@@ -14,9 +16,8 @@ namespace jani_a_varban
         public GraphicsDeviceManager _graphics;
         public GraphicsDevice _device;
         public SpriteBatch _spriteBatch;
-        public const int MapWidth = 15;
-        public const int MapHeight = 15;
-        public Camera _camera;
+        public const int MapWidth = 17;
+        public const int MapHeight = 17;
         public static Vector2 EnteranceCoordinates = new Vector2();
         public static Vector2 ExitCoordinates = new Vector2();
         public static List<Enemy> enemyList = new List<Enemy>();
@@ -32,8 +33,7 @@ namespace jani_a_varban
         {
             _graphics.PreferredBackBufferWidth = 1920;
             _graphics.PreferredBackBufferHeight = 1080;
-            _camera = new Camera(new Vector2(MapHeight*36, MapWidth*36));
-            
+            Camera.Position = new Vector2(MapHeight * 36, MapWidth * 36); // center fix camera on map center
             _graphics.ApplyChanges();
             base.Initialize();
         }
@@ -44,11 +44,8 @@ namespace jani_a_varban
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Entities
-            player = new Player(this);
-            for (int i = 0; i < 3; i++)
-            {
-                enemyList.Add(new Enemy(this));
-            }
+            player = new Player();
+            for (int i = 0; i < 5; i++) enemyList.Add(new Enemy());
 
             // Textures ¯\_(ツ)_/¯
             playerTexture = Content.Load<Texture2D>("hero-down");
@@ -59,22 +56,17 @@ namespace jani_a_varban
         protected override void Update(GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
-            player.HandleInput(!(player.MovementLockDelay == 0), Keyboard.GetState());
-
+            player.HandleInput(!(player.MovementLockDelay == 0), Keyboard.GetState(), Mouse.GetState(Window));
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.DimGray);
-            _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix(GraphicsDevice));
-
+            _spriteBatch.Begin(transformMatrix: Camera.GetViewMatrix(GraphicsDevice));
             Map.DrawMap(_spriteBatch, mapMatrix);
             _spriteBatch.Draw(playerTexture, player.Position, Color.White);
 
-            foreach (var enemy in enemyList)
-            {
-                _spriteBatch.Draw(enemyTexture, enemy.Position, Color.White);
-            }
+            foreach (var enemy in enemyList) _spriteBatch.Draw(enemyTexture, enemy.Position, Color.White);
 
             _spriteBatch.End();
 
