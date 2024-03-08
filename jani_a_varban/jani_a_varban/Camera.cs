ac9d7a0fd95d23448f609f8e2227b29e0482465f
NagyonKaracsony <nagyeliot2004@gmail.com>
2024-03-08 07:56:46 +0100

    updated controls

--- jani_a_varban/jani_a_varban/Camera.cs
+++ jani_a_varban/jani_a_varban/Camera.cs
@@ -3,37 +3,16 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace jani_a_varban
 {
-    public class Camera
+    public static class Camera
     {
-        public Vector2 Position;
-        public float Zoom;
-
-        public Camera(Vector2 position, float zoom = 1.0f)
-        {
-            Position = position;
-            Zoom = zoom;
-        }
-
-        public Matrix GetViewMatrix(GraphicsDevice graphicsDevice)
+        public static Vector2 Position;
+        public static float Zoom = 0.8f;
+        public static int previousScrollValue;
+        public static Matrix GetViewMatrix(GraphicsDevice graphicsDevice)
         {
             return Matrix.CreateTranslation(new Vector3(-Position, 0)) *
                    Matrix.CreateScale(Zoom, Zoom, 1) *
                    Matrix.CreateTranslation(new Vector3(graphicsDevice.Viewport.Width * 0.5f, graphicsDevice.Viewport.Height * 0.5f, 0));
-        }
-
-        public void Move(Vector2 amount)
-        {
-            Position += amount;
-        }
-
-        public void ZoomIn(float amount)
-        {
-            Zoom += amount;
-        }
-
-        public void ZoomOut(float amount)
-        {
-            Zoom -= amount;
         }
     }
 }
