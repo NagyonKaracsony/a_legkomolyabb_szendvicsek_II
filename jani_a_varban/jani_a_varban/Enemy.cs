d34de9524b293d0df88f413551f8053fd91ccb1a
NagyonKaracsony <nagyeliot2004@gmail.com>
2024-03-08 10:52:42 +0100

    pathfinding fixed

--- jani_a_varban/jani_a_varban/Enemy.cs
+++ jani_a_varban/jani_a_varban/Enemy.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 namespace jani_a_varban
 {
     public class Enemy : Game1
@@ -30,81 +29,86 @@ namespace jani_a_varban
                 else invalidSpawn = false;
             }
         }
-        public void Move()
+        public void ApproachPlayer()
         {
             bool intersects = false;
             if (player.Position.X == position.X)
             {
                 if (player.Position.Y > position.Y)
                 {
-                    for (int i = ((int)(position.Y / 72)); i < ((int)(player.Position.Y / 72)); i++)
+                    for (int i = ((int)(position.Y / 72)); i < ((int)(player.Position.Y / 72)) + 1; i++)
                     {
-                        if (mapMatrix[((int)(position.X / 72))][i] == 1) intersects = true;
+                        if (mapMatrix[i][((int)(position.X / 72))] == 1) intersects = true;
                     }
                     if (!intersects) position.Y += 72;
+                    else RandomMove();
                 }
                 else if (player.Position.Y < position.Y)
                 {
-                    for (int i = ((int)(player.Position.Y / 72)); i < ((int)(position.Y / 72)); i++)
+                    for (int i = ((int)(player.Position.Y / 72)); i < ((int)(position.Y / 72)) + 1; i++)
                     {
-                        if (mapMatrix[((int)(position.X / 72))][i] == 1) intersects = true;
+                        if (mapMatrix[i][((int)(position.X / 72))] == 1) intersects = true;
                     }
                     if (!intersects) position.Y -= 72;
+                    else RandomMove();
                 }
             }
             else if (player.Position.Y == position.Y)
             {
                 if (player.Position.X > position.X)
                 {
-                    for (int i = ((int)(position.X / 72)); i < ((int)(player.Position.X / 72)); i++)
+                    for (int i = ((int)(position.X / 72)); i < ((int)(player.Position.X / 72)) + 1; i++)
                     {
-                        if (mapMatrix[1][((int)(position.Y / 72))] == 1) intersects = true;
+                        if (mapMatrix[((int)(position.Y / 72))][i] == 1) intersects = true;
                     }
                     if (!intersects) position.X += 72;
+                    else RandomMove();
+
                 }
                 else if (player.Position.X < position.X)
                 {
-                    for (int i = ((int)(player.Position.X / 72)); i < ((int)(position.X / 72)); i++)
+                    for (int i = ((int)(player.Position.X / 72)); i < ((int)(position.X / 72)) + 1; i++)
                     {
-                        if (mapMatrix[1][(int)(position.Y / 72)] == 1) intersects = true;
+                        if (mapMatrix[((int)(position.Y / 72))][i] == 1) intersects = true;
                     }
                     if (!intersects) position.X -= 72;
+                    else RandomMove();
                 }
-            }
-            else
+            } else RandomMove();
+        }
+        public void RandomMove()
+        {
+            switch (rand.Next(5))
             {
-                switch (rand.Next(5))
-                {
-                    case 0:
-                        if ((int)(position.Y / 72) != 0)
-                        {
-                            if ((mapMatrix[(int)(position.Y / 72) - 1][(int)(position.X / 72)] == 0)) position.Y -= 72;
-                        }
-                        else Move();
-                        break;
-                    case 1:
-                        if ((int)(position.Y / 72) != mapMatrix.Count - 1)
-                        {
-                            if ((mapMatrix[(int)(position.Y / 72) + 1][(int)(position.X / 72)] == 0)) position.Y += 72;
-                            else Move();
-                        }
-                        break;
-                    case 2:
-                        if ((int)(position.X / 72) != 0)
-                        {
-                            if ((mapMatrix[(int)(position.Y / 72)][(int)(position.X / 72) - 1] == 0)) position.X -= 72;
-                        }
-                        else Move();
-                        break;
-                    case 3:
-                        if ((int)(position.X / 72) != mapMatrix.Count - 1)
-                        {
-                            if ((mapMatrix[(int)(position.Y / 72)][(int)(position.X / 72) + 1] == 0)) position.X += 72;
-                        }
-                        else Move();
-                        break;
-                    default: break;
-                }
+                case 0:
+                    if ((int)(position.Y / 72) != 0)
+                    {
+                        if ((mapMatrix[(int)(position.Y / 72) - 1][(int)(position.X / 72)] == 0)) position.Y -= 72;
+                    }
+                    else RandomMove();
+                    break;
+                case 1:
+                    if ((int)(position.Y / 72) != mapMatrix.Count - 1)
+                    {
+                        if ((mapMatrix[(int)(position.Y / 72) + 1][(int)(position.X / 72)] == 0)) position.Y += 72;
+                    }
+                    else RandomMove();
+                    break;
+                case 2:
+                    if ((int)(position.X / 72) != 0)
+                    {
+                        if ((mapMatrix[(int)(position.Y / 72)][(int)(position.X / 72) - 1] == 0)) position.X -= 72;
+                    }
+                    else RandomMove();
+                    break;
+                case 3:
+                    if ((int)(position.X / 72) != mapMatrix.Count - 1)
+                    {
+                        if ((mapMatrix[(int)(position.Y / 72)][(int)(position.X / 72) + 1] == 0)) position.X += 72;
+                    }
+                    else RandomMove();
+                    break;
+                default: break;
             }
         }
     }
