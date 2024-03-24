299cfcabffa4d8e175db646d4f1bdd7bf559a261
NagyonKaracsony <nagyeliot2004@gmail.com>
2024-03-24 08:03:01 +0100

    player logic updated

--- jani_a_varban/jani_a_varban/Player.cs
+++ jani_a_varban/jani_a_varban/Player.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 namespace jani_a_varban
 {
     public class Player : Game1
     {
         public int MovementLockDelay = 60; // ~ 1 sec movement delay at start
-        public int hp = 1000;
-        public int dmg = 350;
-        public Vector2 position = Game1.EnteranceCoordinates;
+        public int hp = 2000;
+        public int dmg = 500;
+        public int dp = 50;
+        public static bool HasKey = false;
+        public static Vector2 position = EnteranceCoordinates;
         public Vector2 Position
         {
             get { return position; }
@@ -20,14 +21,14 @@ namespace jani_a_varban
         {
             if (direction == "up")
             {
-                if ((int) (position.Y / 72) != 0)
+                if ((int)(position.Y / 72) != 0)
                 {
                     if ((mapMatrix[(int)(position.Y / 72) - 1][(int)(position.X / 72)] == 0)) position.Y -= 72;
                 }
             }
             else if (direction == "down")
             {
-                if ((int)(position.Y / 72) != mapMatrix.Count-1)
+                if ((int)(position.Y / 72) != mapMatrix.Count - 1)
                 {
                     if ((mapMatrix[(int)(position.Y / 72) + 1][(int)(position.X / 72)] == 0)) position.Y += 72;
                 }
@@ -41,15 +42,21 @@ namespace jani_a_varban
             }
             else if (direction == "right")
             {
-                if ((int)(position.X / 72) != mapMatrix.Count-1)
+                if ((int)(position.X / 72) != mapMatrix.Count - 1)
                 {
                     if ((mapMatrix[(int)(position.Y / 72)][(int)(position.X / 72) + 1] == 0)) position.X += 72;
+                    if (position.X == ((MapWidth -1) * 72) && Player.HasKey)
+                    {
+                        Map.LoadNextLevel(difficulty += 1);
+                        HasKey = false;
+                        player.hp = 2000;
+                    }
                 }
             }
-            this.MovementLockDelay = 5;
+            this.MovementLockDelay = 15;
 
             CheckForEnemy(true);
-            foreach (var enemy in enemyList) enemy.ApproachPlayer();
+            ProcessEnemies();
             CheckForEnemy(false);
         }
         public Player()
@@ -66,41 +73,71 @@ namespace jani_a_varban
                 else if (keyboard.IsKeyDown(Keys.S) || keyboard.IsKeyDown(Keys.Down)) Move("down");
                 else if (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.Left)) Move("left");
                 else if (keyboard.IsKeyDown(Keys.D) || keyboard.IsKeyDown(Keys.Right)) Move("right");
+                else if (keyboard.IsKeyDown(Keys.E))
+                {
+                    // limit test frequency shit
+                    this.MovementLockDelay = 50;
+                    Map.LoadNextLevel(difficulty);
+                    difficulty += 1;
+                }
             }
-            if (mouse.ScrollWheelValue > Camera.previousScrollValue)
-            {
-                Camera.Zoom = MathHelper.Clamp(Camera.Zoom += 0.075f, 0.75f, 1.25f);
-            }
-            else if (mouse.ScrollWheelValue < Camera.previousScrollValue)
-            {
-                Camera.Zoom = MathHelper.Clamp(Camera.Zoom -= 0.075f, 0.75f, 1.25f);
-            }
+            // egyszerűen nem lehet megoldani a dimm effect arányát //TODO: fix
+            /*
+            if (mouse.ScrollWheelValue > Camera.previousScrollValue) Camera.Zoom = MathHelper.Clamp(Camera.Zoom += 0.075f, 0.5f, 1.0f);
+            else if (mouse.ScrollWheelValue < Camera.previousScrollValue) Camera.Zoom = MathHelper.Clamp(Camera.Zoom -= 0.075f, 0.5f, 1.0f);
             Camera.previousScrollValue = mouse.ScrollWheelValue;
+            */
         }
         public void CheckForEnemy(bool enemyIsTarget)
         {
             for (int i = 0; i < enemyList.Count; i++)
             {
                 var enemy = enemyList[i];
-                if (enemy.position == Position) Battle(enemyIsTarget, enemy);
+                if (enemy.position == player.Position) Battle(enemyIsTarget, enemy);
             }
         }
+        // kétszer fut le, 2x damage a playerre, talán az enemyre is? //TODO: fix
         public void Battle(bool enemyIsTarget, Enemy target)
         {
             if (enemyIsTarget)
             {
                 target.hp -= dmg;
-                hp -= target.dmg;
+                if (target.hp <= 0)
+                {
+                    if (target.HasKey) Player.HasKey = true;
+                    enemyList.Remove(target);
+                }
+                else
+                {
+                    hp -= target.dmg;
+                    if (hp <= 0)
+                    {
+                        Debug.WriteLine("u died");
+                        difficulty = 3;
+                        Map.LoadNextLevel(difficulty);
+                    }
+                }
             }
             else
             {
                 hp -= target.dmg;
+                if (hp <= 0)
+                {
+                    Debug.WriteLine("u died");
+                    difficulty = 3;
+                    Map.LoadNextLevel(difficulty);
+                }
+                else if (target.hp <= 0)
+                {
+                    if (target.HasKey) Player.HasKey = true;
+                    enemyList.Remove(target);
+                }
                 target.hp -= dmg;
             }
-            if (target.hp <= 0) enemyList.Remove(target);
-            if (hp <= 0) Debug.WriteLine("u died");
-            Debug.WriteLine(hp);
-            Debug.WriteLine(target.hp);
+        }
+        public static void ProcessEnemies()
+        {
+            for (int i = 0; i < enemyList.Count; i++) enemyList[i].ApproachPlayer();
         }
     }
 }
